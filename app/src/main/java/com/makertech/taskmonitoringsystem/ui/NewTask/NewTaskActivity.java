955b00e2e85e18b5c73a969cd5c9cccb9095d4dc
Bharat <bk9735732777@gmail.com>
2021-09-22 14:26:11 +0530

    Initial Commit

--- app/src/main/java/com/makertech/taskmonitoringsystem/ui/NewTask/NewTaskActivity.java
+++ app/src/main/java/com/makertech/taskmonitoringsystem/ui/NewTask/NewTaskActivity.java
@@ -1,15 +1,22 @@
 package com.makertech.taskmonitoringsystem.ui.NewTask;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.makertech.taskmonitoringsystem.BR;
 import com.makertech.taskmonitoringsystem.R;
 import com.makertech.taskmonitoringsystem.databinding.NewTaskActivityBinding;
 import com.makertech.taskmonitoringsystem.ui.base.BaseActivity;
 
-import java.util.prefs.BackingStoreException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class NewTaskActivity extends BaseActivity<NewTaskActivityBinding,NewActivityViewModel> {
+
     @Override
     protected void initView() {
+        setSpinnerValue(createYearList(),getViewBinding().yearSpinner);
 
     }
 
@@ -26,5 +33,24 @@ public class NewTaskActivity extends BaseActivity<NewTaskActivityBinding,NewActi
     @Override
     protected Integer getLayoutId() {
         return R.layout.new_task_activity;
+    }
+
+    public List<String> createYearList(){
+        List<String> yearList = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        int currentYear =  calendar.get(Calendar.YEAR);
+        for (int i = currentYear; i<=2080; i++)
+        {
+            yearList.add(String.valueOf(i));
+        }
+
+        return yearList;
+    }
+
+    public void setSpinnerValue(List<String> dataList , Spinner spinner)
+    {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.spin_text_view, dataList);
+        arrayAdapter.setDropDownViewResource(R.layout.spin_dropdown_text_view);
+        spinner.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/makertech/taskmonitoringsystem/ui/home/HomeActivity.java
+++ app/src/main/java/com/makertech/taskmonitoringsystem/ui/home/HomeActivity.java
@@ -1,20 +1,26 @@
 package com.makertech.taskmonitoringsystem.ui.home;
 
 import android.content.Intent;
+import android.os.Build;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.makertech.taskmonitoringsystem.BR;
 import com.makertech.taskmonitoringsystem.R;
 import com.makertech.taskmonitoringsystem.databinding.HomeActivityBinding;
+import com.makertech.taskmonitoringsystem.ui.NewTask.NewTaskActivity;
 import com.makertech.taskmonitoringsystem.ui.base.BaseActivity;
 
 public class HomeActivity extends BaseActivity<HomeActivityBinding,HomeViewModel> {
     @Override
     protected void initView() {
+
         getViewBinding().newTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
+                Intent i = new Intent(getApplicationContext(), NewTaskActivity.class);
+                startActivity(i);
             }
         });
     }
@@ -33,4 +39,6 @@ public class HomeActivity extends BaseActivity<HomeActivityBinding,HomeViewModel
     protected Integer getLayoutId() {
         return R.layout.home_activity;
     }
+
+
 }
